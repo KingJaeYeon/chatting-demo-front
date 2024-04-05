b0f20e6e14684b39929a8ae998364a91c0d89a7e
pjy <wodus331@vanillacode.net>
2024-04-05 22:25:23 +0900

    react route dom RouterProvider 적용중...

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
-import { Register } from "./page/Register.tsx";
 import "./style.scss";
-import { Login } from "./page/Login.tsx";
 import { Home } from "./page/Home.tsx";
 
 import { AuthContextProvider } from "./components/AuthContext.tsx";
 import { useAuthContext } from "./store/authStore.ts";
+import { Login } from "./page/Login.tsx";
+import { Register } from "./page/Register.tsx";
 
-const router = createBrowserRouter([
-  { path: "/", Component: Home, index: true },
-  { path: "/login", Component: Login },
-  { path: "/register", Component: Register },
-]);
-function App() {
+const PrivateRoute = ({ children }: { children: any }) => {
   const { user } = useAuthContext();
-  console.log("user::", user);
+  return user ? children : <Navigate to="/login" />;
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route
+        path={"/"}
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
+      ,
+      <Route path={"/login"} element={<Login />} />,
+      <Route path={"/register"} element={<Register />} />,
+    </>,
+  ),
+);
+function App() {
   return (
     <AuthContextProvider>
       <RouterProvider router={router} />
