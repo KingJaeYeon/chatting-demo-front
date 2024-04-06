1ce26699425771fb8be35ab46e17e5f0eb2c439d
pjy <wodus331@vanillacode.net>
2024-04-06 14:55:17 +0900

    내가 추가한 유저리스트 채팅방에 실시간으로 보여줌

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,16 +1,38 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase.ts";
+import { useEffect, useState } from "react";
+import { useAuthContext } from "../store/authStore.ts";
+
 export function Chats() {
+  const { user } = useAuthContext();
+  const [chats, setChats] = useState<any>([]);
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", user.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    };
+    user.uid && getChats();
+  }, [user.uid]);
+  console.log(Object.entries(chats));
   return (
     <div className={"chats"}>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/18151445/pexels-photo-18151445.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+      {Object.entries(chats)?.map((chat: any) => (
+        <div className="userChat" key={chat[0]}>
+          <img
+            src={chat[1]?.userInfo?.photoURL}
+            alt="img"
+            style={{ background: "white" }}
+          />
+          <div className="userChatInfo">
+            <span>{chat[1]?.userInfo?.displayName}</span>
+            <p>{chat[1]?.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
