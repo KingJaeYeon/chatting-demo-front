c9fe31f65054b988c79a1d2d7279d3d49f1f3450
GuGura <wodus331@gmail.com>
2024-04-10 20:26:13 +0900

    css bug fix

--- src/components/home/friend/request/RequestListContainer.tsx
+++ src/components/home/friend/request/RequestListContainer.tsx
@@ -1,14 +1,12 @@
 import RequestList from "@/components/home/friend/request/RequestList.tsx";
 export default function RequestListContainer({ data }: { data: any }) {
   return (
-    <div>
+    <div className={"w-full "}>
       <p className="text-sm text-muted-foreground transition-colors hover:text-foreground pt-2 pb-4">
         {`친구 요청 목록 (${data.length})`}
       </p>
       <div
-        className={
-          "relative overflow-scroll h-[calc(100%-70px)] w-[1200px] scroll"
-        }
+        className={"relative overflow-scroll w-full h-[calc(100%-70px)] scroll"}
       >
         <div className={"absolute max-w-[1200px] w-full"}>
           <RequestList data={data} />
